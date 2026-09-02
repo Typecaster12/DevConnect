3eee04bac627e6e44726699b78c306661ad86bd7
Harsh Mishra <harshmishra2997@gmail.com>
2026-09-03 00:23:50 +0530

    login api created and tested

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,8 @@
 import User from "../models/Users.model.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+//for new registration;
 export const registerNewUser = async (req, res) => {
     try {
         const { firstName, lastName, username, email, password } = req.body;
@@ -21,7 +23,7 @@ export const registerNewUser = async (req, res) => {
         });
 
         const isUserNameAlreadyTaken = await User.findOne({
-            username
+            "personalInfo.username": username
         });
 
         //error if already taken;
@@ -63,9 +65,71 @@ export const registerNewUser = async (req, res) => {
             message: err.message,
         })
     }
-
-
-
-
 };
 
+//for login existing user;
+export const userLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        //find user in mongodb;
+        const user = await User.findOne({
+            email
+        });
+
+        //validation;
+        if (!user) {
+            return res.status(401).json({
+                message: "Invalid email or password."
+            });
+        }
+
+        //if user exists, compare the password;
+        //to compare plain pass with hashed
+        const isPasswordCorrect = await bcrypt.compare( //will return true or false;
+            password,
+            user.password
+        );
+
+        //validation;
+        if (!isPasswordCorrect) {
+            return res.status(401).json({
+                message: "Invalid username or password"
+            });
+        }
+
+        //if password is correct, generate jwt;
+        const token = jwt.sign(
+            {
+                //id of user, who currently logged in;
+                id: user._id
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1d" //this token will expire in 1 day;
+            }
+        );
+
+
+        // store the token in cookie
+        //this cookie will be sent with every future requests;
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "lax",
+            maxAge: 24 * 60 * 60 * 1000
+        });
+
+        //send the final response;
+        res.status(200).json({
+            status: "Success",
+            message: "Login Successfull, Welcome User."
+        })
+    } catch (err) {
+        res.status(500).json({
+            status: "Failed",
+            message: err.message,
+        })
+    }
+
+}
--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { registerNewUser } from "../controllers/authController.js";
+import { registerNewUser, userLogin } from "../controllers/authController.js";
 
 const authRouter = express.Router();
 
 //registration router;
 authRouter.post("/registration", registerNewUser);
+authRouter.post("/login", userLogin);
+
 export default authRouter;
